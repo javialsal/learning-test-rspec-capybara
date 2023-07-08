cd993dd523d639f66561b39d44c5c012a73497ba
Javier Alvarez <javialsal@gmail.com>
2023-07-08 15:34:06 +0200

    Allow todos to be completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'todos#index'
 
-  resources :todos, only: [:index, :new, :create]
+  resources :todos, only: [:index, :new, :create] do
+    resource :completion, only: :create
+  end
 
   resources :sessions, only: [:new, :create]
 end
