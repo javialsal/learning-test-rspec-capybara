2956b9f98a371e83d9e2b56405859e65817e5929
Javier Alvarez <javialsal@gmail.com>
2023-07-10 13:12:11 +0200

    Refactored todo completions

--- spec/support/features/todo_helper.rb
+++ spec/support/features/todo_helper.rb
@@ -13,3 +13,15 @@ module Features
     have_css ".todos li.completed", text: todo_title
   end
 end
+
+describe Todo, "#completed!" do
+  it "update completed_at" do
+    todo = Todo.create!(completed_at: nil)
+
+    todo.completed!
+
+    todo.reload
+
+    expect(todo).to be_completed
+  end
+end
